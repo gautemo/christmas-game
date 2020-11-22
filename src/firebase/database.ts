35841c8c1e2495ab034d57500d2a99f02928e8af
Gaute Meek Olsen <gaute.meek@gmail.com>
2020-11-22 22:00:29 +0100

    moved firebase code to ensure user is signed in anonymously

--- src/firebase/database.ts
+++ src/firebase/database.ts
@@ -1,17 +1,18 @@
 import { firebase, getCurrentUser } from './firebase'
 
-const db = firebase.firestore()
-const scores = db.collection('scores')
-
 const leaderboardPlaces = 10;
 
 const addScore = async (name: string, score: number) => {
   await getCurrentUser()
+  const db = firebase.firestore()
+  const scores = db.collection('scores')
   await scores.add({name, score})
 }
 
 const getScores = async () => {
   await getCurrentUser()
+  const db = firebase.firestore()
+  const scores = db.collection('scores')
   const scoresSnapshot = await scores.orderBy('score', 'desc').limit(leaderboardPlaces).get()
   return scoresSnapshot.docs.map(doc => ({ ...doc.data(), id: doc.id})) as { name: string, score: number, id: string }[]
 }
